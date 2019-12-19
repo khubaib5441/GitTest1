14f03998e8e3b57a714c6f4a9c9d337034f0dc1f
khubaib <30856607+khubaib5441@users.noreply.github.com>
2019-12-19 11:03:01 +0500

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //code as added in github
             Console.WriteLine("zohaib amin");
         }
     }
